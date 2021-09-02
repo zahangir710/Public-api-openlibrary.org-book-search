f086cc17376580d4361c97210cc3e3dc0f7981ed
zahangir710 <zahangir710@gmail.com>
2021-09-02 12:09:41 +0200

    book list loaded

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,17 @@
-console.log('js file connected');
+// load books
+
+const loadBook=()=>{
+    const searchInput=document.getElementById('search-book').value;
+    const url=`https://openlibrary.org/search.json?q=${searchInput}`;
+    fetch(url)
+    .then(res=> res.json())
+    .then(data=> showData(data.docs));
+}
+
+// display books 
+
+const showData=bookList=>{
+    bookList.forEach(book => {
+        console.log(book.title);    
+    });
+}
