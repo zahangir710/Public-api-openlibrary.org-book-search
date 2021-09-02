e44b928378be747cd71cbd7551ece60638ae24df
zahangir710 <zahangir710@gmail.com>
2021-09-02 19:13:58 +0200

    bug fixed

--- JS/app.js
+++ JS/app.js
@@ -6,13 +6,20 @@ headerMessage.innerText='';
 const spinner=visibility=>{
     document.getElementById('mySpinner').style.display=visibility;
 }
+// hide previous results 
+const previousResults = visibility =>{
+    document.getElementById('search-result').style.display=visibility;
+}
+
 // load books
 const loadBook=()=>{
     spinner('block');
+    previousResults('none');
     const searchInputField=document.getElementById('search-book');
     const searchInput=searchInputField.value;
     if(searchInput.length===0){
         headerMessage.innerText='Please enter a book name to start searching';
+        spinner('none');
     }
     else{
         headerMessage.innerText='';
@@ -41,18 +48,19 @@ const showData=(bookInfo, bookList)=>{
            div.classList.add('col');
            div.innerHTML=`
             <div class="card h-75">
-                <img class="img-thumbnail h-75" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="Book Cover Image">
+                <img class="img-thumbnail h-75" src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top" alt="Book Cover Image">
                 <div class="card-body">
-                    <h3 class="card-title">${book.title}</h3>
-                    <h5 class="card-text">Author: ${book.author_name}</h5>
-                    <p class="card-text">Publisher: ${book.publisher}</p>
+                    <h3 class="card-title">${book?.title}</h3>
+                    <h5 class="card-text">Author: ${book?.author_name}</h5>
+                    <p class="card-text">Publisher: ${book?.publisher}</p>
                 </div>
                  <div class="card-footer">
-                     <small class="text-muted">First Published in: ${book.first_publish_year}</small>
+                     <small class="text-muted">First Published in: ${book?.first_publish_year}</small>
                 </div>
             </div>
            `;
         searchResultDiv.appendChild(div);
         spinner('none');
+        previousResults('flex');
     });
 }
